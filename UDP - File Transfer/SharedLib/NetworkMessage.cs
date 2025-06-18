12bc450bb068bd3f3d27b0310ac0bb493a3d4b56
Iury Faria <iury.gabriel.r@gmail.com>
2025-06-17 21:10:49 -0400

    UDP File Transfer - File problems

---
+++ UDP - File Transfer/SharedLib/NetworkMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace UdpFileTransfer
+{
+    //this is a simple datastructure tath is used in packet queues
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+
+    }
+}
