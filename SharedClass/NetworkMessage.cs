4275ae11c04d704fe9f30821e174a9558121b536
Iury Faria <iury.gabriel.r@gmail.com>
2025-06-18 19:45:22 -0400

    Shared Class (Ball, Packet, Paddle, GeometryGame, etc) done

---
+++ SharedClass/NetworkMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PongGame
+{
+    // Data structure used to store Packets along with their sender
+    public class NetworkMessage
+    {
+        public IPEndPoint Sender { get; set; }
+        public Packet Packet { get; set; }
+        public DateTime ReceiveTime { get; set; }
+    }
+}
---
+++ SharedClass/ThreadSafe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PongGame
+{
+    // Makes one variable thread safe
+    public class ThreadSafe<T>
+    {
+        // The data & lock
+        private T _value;
+        private object _lock = new object();
+
+        // How to get & set the data
+        public T Value
+        {
+            get
+            {
+                lock (_lock)
+                    return _value;
+            }
+
+            set
+            {
+                lock (_lock)
+                    _value = value;
+            }
+        }
+
+        // Initializes the value
+        public ThreadSafe(T value = default(T))
+        {
+            Value = value;
+        }
+    }
+}
